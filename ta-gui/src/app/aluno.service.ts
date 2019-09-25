61510dfa667dd20db9128a4dc8876fadf76f0fe3
Guilhermow <gui.crb0@gmail.com>
2019-09-25 19:11:24 -0300

    refatoracao do metodo gravar -guilhermow

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -4,11 +4,15 @@ export class AlunoService {
     alunos: Aluno[]= []
     gravar(aluno: Aluno){
         var result = false;
-        if(!this.alunos.find(a=> a.cpf == aluno.cpf)){
+        if (this.cpfNaoCadastrado(aluno.cpf)) {
             this.alunos.push(aluno)
             result = true
         }
         return result;
     }
+
+    cpfNaoCadastrado(cpf: string): boolean {
+        return !this.alunos.find(a => a.cpf == cpf);
+     }
   }
 
